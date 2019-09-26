c732cd028b734f01d22314423458fef58dece0c7
mohamedosama2074 <iosengks74@gmail.com>
2019-09-27 00:41:55 +0200

    Change App language without restart your app using MOLH pod

--- Localization/AppDelegate.swift
+++ Localization/AppDelegate.swift
@@ -7,15 +7,25 @@
 //
 
 import UIKit
-
+import MOLH
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate  , MOLHResetable{
+    
+    func reset() {
+        
+        let rootviewcontroller: UIWindow = ((UIApplication.shared.delegate?.window)!)!
+        let stry = UIStoryboard(name: "Main", bundle: nil)
+        rootviewcontroller.rootViewController = stry.instantiateViewController(withIdentifier: "HomeView")
+    }
+    
 
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        MOLH.shared.activate(true)
+        
         return true
     }
 
--- Localization/ViewController.swift
+++ Localization/ViewController.swift
@@ -7,11 +7,12 @@
 //
 
 import UIKit
+import MOLH
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var welcomeLabel: UILabel!
-    
+    @IBOutlet weak var langSwitch: UISwitch!
     @IBOutlet weak var apiReultLabel: UILabel!
     @IBOutlet weak var usernameTF: UITextField!
     
@@ -23,6 +24,27 @@ class ViewController: UIViewController {
         
         
         apiReultLabel.text = "lang".localized == "en" ? dic["en"] : dic["ar"]
+        
+        langSwitch.addTarget(self, action: #selector(changeLang), for: .valueChanged)
+        
+        
+    }
+    
+    
+    @objc func changeLang(){
+        
+        if langSwitch.isOn == true{
+            
+            print("turn on")
+            MOLH.setLanguageTo(MOLHLanguage.currentAppleLanguage() == "en" ? "ar" : "en")
+            MOLH.reset()
+            
+        }else{
+            
+           
+            MOLH.setLanguageTo(MOLHLanguage.currentAppleLanguage() == "ar" ? "en" : "ar")
+            MOLH.reset()
+        }
         
         
     }
