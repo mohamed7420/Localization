89fb39c640bfe0a6709114476b494e150ec3bf7e
mohamedosama2074 <iosengks74@gmail.com>
2019-09-26 23:36:34 +0200

    Localization

--- Localization/ViewController.swift
+++ Localization/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    @IBOutlet weak var apiReultLabel: UILabel!
+    @IBOutlet weak var usernameTF: UITextField!
+    
+    var dic:[String:String] = ["ar":"مصر" , "en":"Egypt"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        apiReultLabel.text = "lang".localized == "en" ? dic["en"] : dic["ar"]
+        
+        
     }
+    
+    
+    @IBAction func btn_Alert(_ sender: UIButton) {
+        
+        let alertController = UIAlertController(title: "Alert".localized,
+                                                message: "message".localized,
+                                                preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: NSLocalizedString("OK".localized, comment: ""), style: .default, handler: nil)
+        
+        alertController.addAction(action)
+        
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+    
 
+}
 
+extension String {
+    
+    var localized:String{
+        
+        return NSLocalizedString(self, comment: "")
+    }
+   
+    
 }
 
